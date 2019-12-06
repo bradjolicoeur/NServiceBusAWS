b20e8aa7f912b273563e905add6e3a0d9fc80ef8
bjolicoeur <brad@jolicoeurs.net>
2019-12-05 20:57:38 -0500

    added unit test project

---
+++ src/MyEndpointTests/UnitTest1.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
+using NServiceBus.Testing;
+using MyMessages.Commands;
+using MyEndpoint.Handlers;
+
+namespace MyEndpointTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public async Task ShouldReplyWithResponseMessage()
+        {
+            var handler = new HandleMyMessage();
+            var context = new TestableMessageHandlerContext();
+
+            await handler.Handle(new MyMessage(), context)
+                .ConfigureAwait(false);
+
+            Assert.Empty(context.RepliedMessages);
+            //Assert.IsInstanceOf<MyResponse>(context.RepliedMessages[0].Message);
+        }
+    }
+}
